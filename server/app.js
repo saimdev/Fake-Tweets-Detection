5213a48e61b63e92c9c3044968912f15c9c153e3
saimdev <saimabbasi486@gmail.com>
2023-04-24 06:18:15 -0700

    Changes

--- server/app.js
+++ server/app.js
@@ -1,12 +1,21 @@
+// requiring mongoose for database
+const mongoose = require("mongoose");
 // requiring express
 const express = require('express')
 // putting all express functions in constant app
 const app = express();
 
+const DB="mongodb+srv://sabbas486249:Raza5085458@cluster0.99oj3k2.mongodb.net/mlproject?retryWrites=true&w=majority"
+
+mongoose.connect(DB).then(()=>{
+  console.log("DB CONNECTED");
+}).catch((err)=>{console.log(err)})
+
 // middleware has one more parameter which is next like where
 // to go after we get responses
 const middleware = (req, res, next)=>{
   console.log("Hello Middleware")
+  next();
 }
 
 
@@ -16,6 +25,10 @@ const middleware = (req, res, next)=>{
 app.get('/', (req, res) => {
   res.send('GET request to the homepage')
 });
+
+app.get('/about', middleware, (req, res) => {
+  res.send('GET request to the aboutpage')
+})
 
 // now to visualize that response of server
 app.listen(3000, ()=>{
