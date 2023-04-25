92a50c2718bef1400f7cab668b6ee206aeb0547f
saimdev <saimabbasi486@gmail.com>
2023-04-25 11:15:36 -0700

    Login Function

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -10,7 +10,7 @@ router.post('/register', (req,res)=>{
     const {username, email, password} = req.body;
     const reports=0;
     if(!username || !email || !password){
-        return res.status(422).json({error: "Plzz filled misisng fields"});
+        return res.status(422).json({error: "Plzz fill misisng fields"});
     }
 
     User.findOne({email:email})
@@ -27,6 +27,22 @@ router.post('/register', (req,res)=>{
         
     }).catch((err)=>{console.log(err);})
     console.log(req.body);
+});
+
+router.post("/signin", (req, res)=>{
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        return res.status(422).json({error:"plzz fill missing fields"});
+    }
+
+    User.findOne({email:email})
+    .then((checkUser)=>{
+        if(checkUser){
+            return res.status(201).json({message:"Logged in successfully"});
+        }
+        res.status(400).json({error:"User not found"});
+    }).catch((err)=>{ console.log(err) })
 });
 
 module.exports = router;
